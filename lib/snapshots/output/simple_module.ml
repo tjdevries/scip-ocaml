7036969f67058b7938ed98097c4a6977034145dd
TJ DeVries <devries.timothyj@gmail.com>
2023-04-03 17:40:15 -0400

    init: working on branch

---
+++ lib/snapshots/output/simple_module.ml
@@ -0,0 +1,43 @@
+  module WithFunction = struct
+(*       ^^^^^^^^^^^^ definition scip-ocaml opam . . simple_module/WithFunction# *)
+    type t =
+      { x : int
+      ; y : int
+      }
+  
+    let read () = print_endline "read"
+(*      ^^^^ definition scip-ocaml opam . . simple_module/WithFunction#read(). *)
+    let init () = { x = 1; y = 2 }
+(*      ^^^^ definition scip-ocaml opam . . simple_module/WithFunction#init(). *)
+    let add t i = { t with x = t.x + i }
+(*      ^^^ definition scip-ocaml opam . . simple_module/WithFunction#add(). *)
+    let sub t i = { t with y = t.y - i }
+(*      ^^^ definition scip-ocaml opam . . simple_module/WithFunction#sub(). *)
+  end
+  
+  let a = WithFunction.init ()
+(*        ^^^^^^^^^^^^^^^^^ reference scip-ocaml opam . . simple_module/WithFunction#init(). *)
+  let b = WithFunction.add a 1
+(*        ^^^^^^^^^^^^^^^^ reference scip-ocaml opam . . simple_module/WithFunction#add(). *)
+  let c = WithFunction.sub b 2
+(*        ^^^^^^^^^^^^^^^^ reference scip-ocaml opam . . simple_module/WithFunction#sub(). *)
+  let _ = c
+  let read () = "read"
+(*    ^^^^ definition scip-ocaml opam . . simple_module/read(). *)
+  let _ = read ()
+(*        ^^^^ reference scip-ocaml opam . . simple_module/read(). *)
+  
+  let () =
+    let _ = WithFunction.read () in
+(*          ^^^^^^^^^^^^^^^^^ reference scip-ocaml opam . . simple_module/WithFunction#read(). *)
+    print_endline "done"
+  ;;
+  
+  let () =
+    let open WithFunction in
+    let _ = read () in
+(*          ^^^^ reference scip-ocaml opam . . simple_module/WithFunction#read(). *)
+    print_endline "done"
+  ;;
+  
+  
